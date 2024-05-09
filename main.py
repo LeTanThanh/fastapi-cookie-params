63bbda12cdca64c3d02dd62de86af408707841a0
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-10 02:42:17 +0700

    Set / Get cookie

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi import Response
 from fastapi import Cookie
+from uuid import uuid1
 
 from typing import Annotated
 
 app = FastAPI()
 
-@app.get("/items")
-async def read_items(ads_id: Annotated[str | None, Cookie()] = None):
-  return {"ads_id": ads_id}
+@app.post("/cookie/example")
+async def create_example_cookie(response: Response):
+  response.set_cookie(key = "example", value = uuid1())
+  return {"success": True}
+
+@app.get("/cookie/example")
+async def read_example_cookie(example: Annotated[str | None, Cookie()] = None):
+  return {"example": example}
